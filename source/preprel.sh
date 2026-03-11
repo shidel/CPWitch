93f118562db72c9ca99b7bd45baa787ee22a58d1
Shidel <jerome@shidel.net>
2026-03-11 13:10:33 -0400

    preparing for preview release

--- source/preprel.sh
+++ source/preprel.sh
@@ -37,7 +37,7 @@ cp -f CPWitch  ../bin/macOS/CPWitch.app/Contents/MacOS/
 cp -f codepages.ini  ../bin/macOS/CPWitch.app/Contents/Resources/
 cp -f translations/*.nls ../bin/macOS/CPWitch.app/Contents/Resources/
 cp -f 0816norm.uff ../bin/macOS/CPWitch.app/Contents/Resources/
-cp -f dictionary.cpw ../bin/macOS/CPWitch.app/Contents/Resources/
+cp -f master.cpw ../bin/macOS/CPWitch.app/Contents/Resources/
 cp -f TODO.md ../bin/macOS/
 
 # Linux Release files excluding binary
@@ -45,7 +45,7 @@ echo 'Update Linux Package data files'
 cp -f codepages.ini  ../bin/Linux/
 cp -f translations/*.nls ../bin/Linux/
 cp -f 0816norm.uff ../bin/Linux/
-cp -f dictionary.cpw ../bin/Linux/
+cp -f master.cpw ../bin/Linux/
 cp -f TODO.md ../bin/Linux/
 
 # Windows Release files excluding binary
@@ -53,7 +53,7 @@ echo 'Update Windows Package data files'
 cp -f codepages.ini  ../bin/Windows/
 cp -f translations/*.nls ../bin/Windows/
 cp -f 0816norm.uff ../bin/Windows/
-cp -f dictionary.cpw ../bin/Windows/
+cp -f master.cpw ../bin/Windows/
 cp -f TODO.md ../bin/Windows/
 
 build_flag DEBUG
